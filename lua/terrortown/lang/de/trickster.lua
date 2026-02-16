a040e388699b7f244feb84bd017e05a2a9ed43b5
grasotus <grasotus@googlemail.com>
2026-02-16 17:26:48 +0100

    update trickster wording

--- lua/terrortown/lang/de/trickster.lua
+++ lua/terrortown/lang/de/trickster.lua
@@ -3,9 +3,9 @@ local L = LANG.GetLanguageTableReference("de")
 -- GENERAL ROLE LANGUAGE STRINGS
 
 L[TRICKSTER.name] = "Trickser"
-L["info_popup_" .. TRICKSTER.name] = [[Du bist der Trickser! Nach deinem Tod siehst du aus wie ein Unschuldiger.]]
+L["info_popup_" .. TRICKSTER.name] = [[Du bist der Trickser! Lebendig und tot siehst du aus wie ein Unschuldiger.]]
 L["target_" .. TRICKSTER.name] = "Trickser"
-L["ttt2_desc_" .. TRICKSTER.name] = "Der Trickser ist ein Verräter, der nach seinem Tod wie ein Unschuldiger aussieht."
+L["ttt2_desc_" .. TRICKSTER.name] = "Der Trickser ist ein Verräter, der lebendig und tot wie ein Unschuldiger aussieht."
 L["body_found_" .. TRICKSTER.abbr] = "Er war ein Trickser!"
 L["search_role_" .. TRICKSTER.abbr] = "Diese Person war ein Trickser!"
 L["trickster_love_faded"] = "Deine Liebe ist eine Illusion."
